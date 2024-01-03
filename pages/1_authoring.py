30b332d2e8681fa26eeb0c9ad646de4fad01d1aa
Yilu Sun <yiluaq26j@gmail.com>
2024-01-03 14:45:08 -0800

    Update 1_authoring.py

--- pages/1_authoring.py
+++ pages/1_authoring.py
@@ -69,45 +69,6 @@ with tab1:
             st.write(st.session_state.result)
         persona = st.session_state.result
 
-        # if st.button("Auto-generate", key="generate"):
-        #     persona = draft_persona(name, age, gender, occupation)
-        # st.write(persona)
-
-
-with tab2:
-    st.header("Training Setup")
-    col3_1, col3_2 = st.columns(2)
-    with col3_1:
-        training_scenario = st.text_area("Training Scenario")
-        difficulty = st.selectbox("Difficulty level", ("Practice", "Application"))
-        training_type = st.selectbox("Training Types", ("Full Conversation", "Skill Based", "Contextual"))
-
-        if "Skill Based" in training_type:
-            selected_skill = st.selectbox("The skill to be trained on", ("Acknowledging perspective", "Managing expectation",
-                                                                  "Purposeful questioning", "Providing feedback"))
-    with col3_2:
-        st.markdown("Conversation Flow Setup")
-        step1 = st.multiselect("Step 1: How to start the conversation?", ("Set the stage", "Ask a question to prompt reflection"))
-        step2 = st.multiselect("Step 2: How to pave the conversation for coaching?", (
-        "Acknowledging perspective", "Prompt for more questions to dig deeper",
-        "None"))
-        step3 = st.multiselect("Step 3: How to provide feedback?", (
-        "Ask a question about the situation", "Ask a question about the behavior", "Ask a question about the impact",
-        "Provide feedback on situation", "Provide feedback on behavior", "Provide feedback on impact"))
-        step4 = st.multiselect("Step 4: How to provide resolution or next steps?", ("Ask questions to come up with resolutions", "Share possible resolutions to improve"))
-        step5 = st.multiselect("Step 5: How to end the conversation?", ("Re-iterate on next steps", "None"))
-
-with st.sidebar:
-    st.header("Character and Training Scenario Setup")
-    st.markdown('''
-                Please create the character and training setup tabs to setup the soft skill training.  \n
-                When you finish, please download the json file with the button below:''')
-    prompt = assemble_prompt(name, persona, training_scenario, step1, step2, step3, step4, step5)
-    
-    output = assemble_output(prompt, convo_index, convo_type)
-    
-
-
     st.download_button(
         label="Download",
         data=output.to_csv().encode('utf-8'),
